a59c2305ce514f17f9490b6d8cdd36afe39b43a8
linx-235 <xyfernx@gmail.com>
2023-06-23 19:54:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from pygame import *
 
 init()
 
+class Currency(object):
+    def __int__ (self, value, name, image_frames, cap):
+        self.value = value
+        self.name = name
+        self.image_frames = image_frames
+        self.cap = cap
+
+class Building(object):
+    def __int__ (self, value, name, image_frames, cap):
+        self.value = value
+        self.name = name
+        self.image_frames = image_frames
+        self.cap = cap
+
+
 width = 1000
 height = 600
 screen = display.set_mode((width, height))
@@ -9,5 +24,12 @@ clock = time.Clock()
 
 run = True
 
+#1 is start page, 2 is game
+curState = 1
+
 while run:
-    #test again
+    for e in event.get():
+        if e.type == MOUSEBUTTONDOWN:
+            run = False
+    display.flip()
+    clock.tick(60)
