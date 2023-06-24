c9c3cd5b299e4735acec00d87acf62d7dc7cdc95
linx-235 <xyfernx@gmail.com>
2023-06-23 20:55:52 -0400

    starting grid

--- drawFunctions.py
+++ drawFunctions.py
@@ -1,1 +1,10 @@
+from pygame import *
+
+def drawGame(screen, grid):
+    img = transform.scale(image.load("images/borderless_grass.png"), (640, 480))
+    for i in range(51):
+        for j in range(51):
+            if grid[i][j] == 1:
+                continue
+            screen.blit(img, (j * 104 - (i % 2) * 52 - 2000, i * 32))
 
--- main.py
+++ main.py
@@ -18,22 +18,42 @@ class Building(object):
         self.image_frames = image_frames
         self.cap = cap
 
-
-width = 1000
+width = 1500
 height = 600
 screen = display.set_mode((width, height))
 clock = time.Clock()
 
+grid = [[1 for i in range(51)] for j in range(51)]
+
+grid[0][25] = 0
+
+for i in range(1, 51):
+    for j in range(51):
+        grid[i][j] = grid[i - 1][j]
+    if i <= 25:
+        if i % 2 == 0:
+            grid[i][25 - (i + 1) // 2] = 0
+        else:
+            grid[i][25 + (i + 1) // 2] = 0
+    else:
+        if i % 2 == 0:
+            grid[i][50 - (i - 1) // 2] = 1
+        else:
+            grid[i][(i + 1 - 25) // 2 + 12] = 1
+
 run = True
 
 #1 is start page, 2 is game
-curState = 1
+curState = 2
 
 while run:
     for e in event.get():
         if e.type == MOUSEBUTTONDOWN:
             run = False
-    if curState == 1:
-        drawMain
+        if e.type == QUIT:
+            run = False
+    if curState == 2:
+        drawGame(screen, grid)
     display.flip()
     clock.tick(60)
+quit()
