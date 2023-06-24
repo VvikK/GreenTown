f0ff1d594244a15fadd2adfca407765d4144ae8c
linx-235 <xyfernx@gmail.com>
2023-06-23 22:38:29 -0400

    pls faster

--- logicFunctions.py
+++ logicFunctions.py
@@ -1,1 +1,27 @@
+from pygame import *
+
+def hoverDiamond(grid, x, y):
+    for i in range(51):
+        for j in range(51):
+            if grid[i][j] == 1:
+                continue
+            top = i * 28
+            left = j * 98 - (i % 2) * 49 - 2000
+            right = (j + 1) * 98 - (i % 2) * 49 - 2000
+            bot = (i + 2) * 28
+            midheight = (top + bot) // 2
+            midwidth = (left + right) // 2
+            if x < left or x > right:
+                continue
+            if y < top or y > bot:
+                continue
+            if y < midheight:
+                if x >= left + (midheight - y) * 4 // 13 and x <= right - (midheight - y) * 4 // 13:
+                    return i, j
+            else:
+                if x >= left + (y - midheight) * 4 // 13 and x <= right - (y - midheight) * 4 // 13:
+                    return i, j
+    return -1, -1
+
+
 
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logicFunctions import *
 
 init()
 
+WHITE = Color("#FFFFFF")
+
 class Currency(object):
     def __int__ (self, value, name, image_frames, cap):
         self.value = value
@@ -23,23 +25,23 @@ height = 600
 screen = display.set_mode((width, height))
 clock = time.Clock()
 
-grid = [[1 for i in range(51)] for j in range(51)]
+grid = [[1 for i in range(9)] for j in range(9)]
 
-grid[0][25] = 0
+grid[0][4] = 0
 
-for i in range(1, 51):
-    for j in range(51):
+for i in range(1, 9):
+    for j in range(9):
         grid[i][j] = grid[i - 1][j]
-    if i <= 25:
+    if i <= 4:
         if i % 2 == 0:
-            grid[i][25 - (i + 1) // 2] = 0
+            grid[i][4 - (i + 1) // 2] = 0
         else:
-            grid[i][25 + (i + 1) // 2] = 0
+            grid[i][4 + (i + 1) // 2] = 0
     else:
         if i % 2 == 0:
-            grid[i][50 - (i - 1) // 2] = 1
+            grid[i][4 - (i - 1) // 2] = 1
         else:
-            grid[i][(i + 1 - 25) // 2 + 12] = 1
+            grid[i][(i + 1 - 7) // 2 + 4] = 1
 
 run = True
 
@@ -47,13 +49,21 @@ run = True
 curState = 2
 
 while run:
+    
+    x, y = mouse.get_pos()
+    #k, l = hoverDiamond(grid, x, y)
+    #print(x, y, "yes!")
     for e in event.get():
         if e.type == MOUSEBUTTONDOWN:
             run = False
         if e.type == QUIT:
             run = False
+        
     if curState == 2:
         drawGame(screen, grid)
+    #if k != -1:
+    #draw.rect(screen, WHITE, Rect(l * 98 - (k % 2) * 49 - 2000 + 49, k * 28 + 14, 10, 10))
+    draw.rect(screen, WHITE, Rect(x, y, 10, 10))
     display.flip()
     clock.tick(60)
 quit()
