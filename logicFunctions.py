a9bd9dea2a5fd10aef405cae438308c90d350475
linx-235 <xyfernx@gmail.com>
2023-06-24 08:14:27 -0400

    diamond detection

--- logicFunctions.py
+++ logicFunctions.py
@@ -1,5 +1,22 @@
 from pygame import *
 
+def gridCreation(grid):
+    grid[0][25] = 0
+    for i in range(1, 51):
+        for j in range(51):
+            grid[i][j] = grid[i - 1][j]
+        if i <= 25:
+            if i % 2 == 0:
+                grid[i][25 - (i + 1) // 2] = 0
+            else:
+                grid[i][25 + (i + 1) // 2] = 0
+        else:
+            if i % 2 == 0:
+                grid[i][51 - (i - 1) // 2 - 1] = 1
+            else:
+                grid[i][(i + 1 - 25) // 2 + 1] = 1
+    return grid
+
 def hoverDiamond(grid, x, y):
     for i in range(51):
         for j in range(51):
@@ -16,10 +33,10 @@ def hoverDiamond(grid, x, y):
             if y < top or y > bot:
                 continue
             if y < midheight:
-                if x >= left + (midheight - y) * 4 // 13 and x <= right - (midheight - y) * 4 // 13:
+                if x >= left + (midheight - y) * 28 / 98 and x <= right - (midheight - y) * 28 / 98:
                     return i, j
             else:
-                if x >= left + (y - midheight) * 4 // 13 and x <= right - (y - midheight) * 4 // 13:
+                if x >= left + (y - midheight) * 98 / 28 and x <= right - (y - midheight) * 98 / 28:
                     return i, j
     return -1, -1
 
