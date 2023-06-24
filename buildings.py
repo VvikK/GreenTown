5b4399384610d518414a3ce609c2f89336e40f9f
VvikK <78823821+VvikK@users.noreply.github.com>
2023-06-24 00:04:16 -0400

    hi

---
+++ buildings.py
@@ -0,0 +1,15 @@
+class Buildings:
+    def __init__(self, name, buildingtype, cost, info, happiness_contribution, frames, co2_contribution, money_contribution):
+        self. name = name
+        self.buildingtype = buildingtype
+        self.cost = cost
+        self.info = info
+        self.happiness_contribution = happiness_contribution
+        self.frames = frames
+        self.co2_contribution = co2_contribution
+        self.money_contribution = money_contribution
+
+
+
+
+
